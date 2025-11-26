48125095d3dd8d97871a1439efe6a3433ed00e94
Danilo <danilo.carrillo02@inacapmail.cl>
2025-11-26 20:37:44 -0300

    CRUD (Editar y Eliminar) en RecepcionApp

--- RecepcionApp/urls.py
+++ RecepcionApp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('registrar/', views.registrar_equipo, name='registrar_equipo'), 
     path('listado/', views.listado_equipos, name='listado_equipos'),    
     path('detalle/<str:nombre>/', views.detalle_equipo, name='detalle_equipo'), 
+    path('eliminar/<int:id>/', views.eliminar_equipo, name='eliminar_equipo'),
+    path('editar/<int:id>/', views.editar_equipo, name='editar_equipo'),
 ]
